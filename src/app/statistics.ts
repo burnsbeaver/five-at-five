c36f266e6437ea7d7062aa494a723cf7a407abd2
burnsbeaver <burnsbeaver@gmail.com>
2020-04-10 18:04:48 -0400

    pledges

--- src/app/statistics.ts
+++ src/app/statistics.ts
@@ -113,5 +113,28 @@ export const stats = [
         par: 5,
         score: 6
       }
-  }
+  },
+  {
+    day: 'April 10th',
+    hole1: {
+      par: 4,
+      score: 4
+    },
+    hole2: {
+      par: 4,
+      score: 3
+    },
+    hole3: {
+      par: 4,
+      score: 4
+    },
+    hole4: {
+      par: 5,
+      score: 5
+    },
+    hole5: {
+      par: 3,
+      score: 4
+    }
+}
 ];
